1365a0415bbf8a9f8a83b34bae6b7401cdff3359
Leandro Guerra <eng.leandroguerra@gmail.com>
2015-03-15 11:44:12 -0300

    Adição de comentários

--- twitterPython.py
+++ twitterPython.py
@@ -10,21 +10,25 @@ import time
 reload(sys) 
 sys.setdefaultencoding("ascii")
 
- 
-def get_tweets(query, max = 3000):
-    # takes a search term (query) and a max number of tweets to find
-    # gets content from twitter and writes it to a csv bearing the name of your query
+#
+def coleta_tweets(query, max = 3000):
+    #recebe o termo de consulta e a quantidade máxima de tweets por busca
+    #escreve os resultados em um arquivo csv, sem sobreposição de dados 
     
+    #contador
     i = 0
+    #recebe o termo de busca
     search = query
- 
-    with open(search+'.csv', 'a') as outf:
-        writer = csv.writer(outf,delimiter='|')
+	
+	#abre um aquivo csv com o nome do termo de busca
+    with open(search+'.csv', 'a') as outf: # o parametro "a" indica que não averá sobreposição
+		#cada novo registro será adicionado ao final do arquivo
+        writer = csv.writer(outf,delimiter='|') #utiliza o | como delimitador de campo
         writer.writerow(['user','time','tweet','latitude','longitude'])
         try:
             tso = TwitterSearchOrder()
             tso.set_keywords([search])
-            tso.set_language('pt') # Portuguese tweets only
+            tso.set_language('pt') # Apenas Tweets em portugues
  
             ts = TwitterSearch(
 				access_token  = '38855125-pNaSiE7TKu4VMl8S41fXKKVYV7qSgYlEtIXswRY8p',
@@ -36,13 +40,13 @@ def get_tweets(query, max = 3000):
             for tweet in ts.search_tweets_iterable(tso):
                 lat = None
                 long = None
-                time = tweet['created_at'] # UTC time when Tweet was created.
+                time = tweet['created_at'] # base UTC
                 user = tweet['user']['screen_name']
                 tweet_text =  unicodedata.normalize('NFKD',tweet['text'].strip()).encode('ascii', 'ignore')
                 #tweet_text =  tweet['text'].strip().encode('ascii', 'ignore')
                 tweet_text = ''.join(tweet_text.splitlines())
                 print i,time,
-                if tweet['geo'] != None and tweet['geo']['coordinates'][0] != 0.0: # avoiding bad values
+                if tweet['geo'] != None and tweet['geo']['coordinates'][0] != 0.0: #tratamos para evitar sujeira
                     lat = tweet['geo']['coordinates'][0]
                     long = tweet['geo']['coordinates'][1]
                     print('@%s: %s' % (user, tweet_text)), lat, long
@@ -54,15 +58,17 @@ def get_tweets(query, max = 3000):
                 if i > max:
                     return()
  
-        except TwitterSearchException as e: # take care of all those ugly errors if there are some
+        except TwitterSearchException as e: #apenas para mostrar o tipo de erro, caso exista
             print(e)
  
-query = raw_input ("Search for: ")
+query = raw_input ("Sua busca: ")
 max_tweets = 3000
 tempo_espera = 60 # Tempo de espera entre uma busca e outra (em segundos)
 quantidade_buscas = 10000
+
+#Loop para busca
 for l in range(1,quantidade_buscas+1):
-	get_tweets(query, max_tweets)
+	coleta_tweets(query, max_tweets)
 	time.sleep(tempo_espera)
 	l += 1
 
